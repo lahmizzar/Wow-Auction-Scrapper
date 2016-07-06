7e700f3a7f7ee2dc0aa10df0a3bc891170fb499c
chuclucillo <chuclucillo@gmail.com>
2016-07-06 02:08:21 +0200

    mejoras controller auctions

--- server/controllers/scrapper.js
+++ server/controllers/scrapper.js
@@ -70,30 +70,42 @@ var task_auctions = function() {
     console.log('-== Begin Get API Auctions ==-');
     var timer= new Date();
     var local_vars = {};
+    local_vars.count=0;
     async.series([
         function(callback_1) {
-            console.log('Asking API for auctions file...')
+            console.log('- Asking API for auctions file...');
             api_client.getAuctions()
                 .then(function (data) {
                     data = JSON.parse(data);
                     if(parseInt(data.files[0].lastModified) <= parseInt(nconf.get('lastupdate:auctiondb'))) return callback_1(new Error('Actions package already processed'));
                     local_vars.api_data = data;
-                    nconf.set('lastupdate:auctiondb', data.files[0].lastModified);
+                    //nconf.set('lastupdate:auctiondb', data.files[0].lastModified);
                     callback_1();
                 });
         },
         function(callback_1) {
-            console.log('Downloading Auctions file...')
+            console.log('- Downloading Auctions file...');
             requests.request(local_vars.api_data.files[0].url)
                 .then(function(data){
-                    console.log('Processing Auctions file...')
+                    console.log('- Processing Auctions file...');
                     data = JSON.parse(data);
-                    local_vars.auctions = data;
-
+                    local_vars.auctions = data.auctions;
+                    async.eachLimit(local_vars.auctions, 4, function(auction, callback_2){
+                        local_vars.count++;
+                        console.log(local_vars.count, '/', local_vars.auctions.length);
+                        callback_2();
+                    }, callback_1);
                 });
+        },
+        function(callback_1) {
+            console.log('- Ending Auctions ended...');
+            callback_1();
         }
-    ],function(){
+    ],function(err, results) {
+        if(err)
+            console.log(err.message);
         console.log('-== End Get API Auctions==-');
+        console.log('Operation elapsed', ((new Date().getTime() - timer.getTime())/1000), 'seconds.');
     });
 };
 
